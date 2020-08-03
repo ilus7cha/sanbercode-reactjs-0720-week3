11712037bdddac445d74dbd4b1020e510ff609e0
dian-anindita <ditaa09@gmail.com>
2020-08-03 22:07:24 +0700

    update tugas 11

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.css';
+import HargaBuah from './tugas11';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Tabel harga Buah</h1>
+      <HargaBuah />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/tugas11/index.js
@@ -0,0 +1,36 @@
+  import React from 'react';
+  import './index.css';
+
+let dataHargaBuah = [
+    {nama: "Semangka", harga: 10000, berat: 1000},
+    {nama: "Anggur", harga: 40000, berat: 500},
+    {nama: "Strawberry", harga: 30000, berat: 400},
+    {nama: "Jeruk", harga: 30000, berat: 1000},
+    {nama: "Mangga", harga: 30000, berat: 500}
+  ]
+
+class HargaBuah extends React.Component {
+  render() {
+    return (
+    
+    <table>
+        <tr>
+            <th>Nama</th>
+            <th>Harga</th>
+            <th>Berat</th>
+        </tr>
+
+        {dataHargaBuah.map(el => {
+            return (
+                <tr>
+                    <td>{el.nama}</td>
+                    <td>{el.harga}</td>
+                    <td>{el.berat / 1000} kg </td>
+                </tr>
+            )
+        })}
+    </table>)
+  }
+}
+
+export default HargaBuah;
